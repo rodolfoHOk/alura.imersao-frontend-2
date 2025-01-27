2ff6fe3b36b96eb0819a70a506c87f5975ad8901
Rodolfo HiOk <rodolfo.okino@gmail.com>
2025-01-27 11:37:42 -0300

    feat: create next app and clean project

---
+++ spotify-with-next/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col w-screen h-screen bg-zinc-950">
+      <main className="flex flex-col h-screen justify-center items-center">
+        <p className="text-zinc-50 font-bold text-2xl">Spotify</p>
+      </main>
+    </div>
+  );
+}
